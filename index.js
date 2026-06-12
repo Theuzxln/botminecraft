ce577d06ecb92236c8e41021a1a928ad10307fc7
Theuzxln <matheusvinicius0986@gmail.com>
2026-06-11 23:14:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,24 @@
-const mineflayer = require('mineflayer');
-const http = require('http');
+const net = require('net');
 
-console.log('Script iniciado');
+console.log('Testando conexão...');
 
-http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('OK');
-}).listen(process.env.PORT || 10000, () => {
-  console.log('HTTP iniciado');
+const socket = net.createConnection({
+  host: 'myworldgbgat.aternos.me',
+  port: 62351
 });
 
-function startBot() {
-  console.log('Iniciando bot...');
-  console.log('HOST =', process.env.HOST);
-  console.log('PORT_MC =', process.env.PORT_MC);
-  console.log('BOT_NAME =', process.env.BOT_NAME);
+socket.on('connect', () => {
+  console.log('PORTA ABERTA');
+  socket.destroy();
+});
 
-  try {
-    console.log('Antes do createBot');
+socket.on('error', (err) => {
+  console.log('ERRO:', err.message);
+});
 
-    const bot = mineflayer.createBot({
-      host: process.env.HOST,
-      port: Number(process.env.PORT_MC),
-      username: process.env.BOT_NAME,
-      auth: 'offline',
-      version: '1.21.1'
-    });
+socket.setTimeout(10000);
 
-    console.log('Depois do createBot');
-
-    bot._client.on('connect', () => {
-      console.log('Socket conectado');
-    });
-
-    bot._client.on('close', () => {
-      console.log('Socket fechado');
-    });
-
-    bot.on('login', () => {
-      console.log('Login realizado');
-    });
-
-    bot.on('spawn', () => {
-      console.log('Bot entrou no servidor');
-    });
-
-    bot.on('error', (err) => {
-      console.log('Erro:', err);
-    });
-
-    bot.on('kicked', (reason) => {
-      console.log('Kickado:', reason);
-    });
-
-    bot.on('end', () => {
-      console.log('Desconectado');
-
-      setTimeout(startBot, 15000);
-    });
-
-  } catch (err) {
-    console.log('Erro fatal:', err);
-  }
-}
-
-startBot();
+socket.on('timeout', () => {
+  console.log('TIMEOUT');
+  socket.destroy();
+});
